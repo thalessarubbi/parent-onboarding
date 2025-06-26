acca0eb94964c202b5f9e5f8b9bc7891ce207f29
Thales Sarubbi <thalessarubbi@gmail.com>
2025-06-26 14:02:29 -0300

    style: :lipstick: set up gluestack ui

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "@/global.css";
+import { GluestackUIProvider } from "@/src/components/ui/gluestack-ui-provider";
+import { Box } from './src/components/ui/box';
+import { Text } from './src/components/ui/text';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { RubikDirt_400Regular, useFonts } from '@expo-google-fonts/rubik-dirt';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    RubikDirt_400Regular
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null;
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider mode="light">
+      <Box className='flex-1 items-center justify-center bg-ginko-primary px-16'>
+        <Text className='text-h1 font-heading text-center color-black fill-black'>Welcome to Ginko!</Text>
+        <StatusBar style="auto" />
+      </Box>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
